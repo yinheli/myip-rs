d96f90ebc97920574a254730c022fb395fbcae85
yinheli <me@yinheli.com>
2022-09-08 17:09:12 +0800

    chore: upgrade

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-use std::net::{TcpStream, ToSocketAddrs};
 use std::time::Duration;
-use std::io::Read;
-use std::process;
 
 fn main() {
-    let addr = "ns1.dnspod.net:6666".to_socket_addrs().unwrap().next().unwrap();
-    let tcp = TcpStream::connect_timeout(&addr, Duration::new(5, 0));
-    let mut tcp = match tcp {
-        Ok(t) => t,
-        Err(e) => {
-            println!("error: {}", e);
-            process::exit(1);
-        }
-    };
-    let mut buf = String::new();
-    tcp.set_read_timeout(Option::Some(Duration::new(5, 0))).unwrap();
-    let size = tcp.read_to_string(&mut buf).unwrap();
-    if size > 0 {
-        println!("{}", buf);
-    } else {
-        println!("read error");
-        process::exit(1);
+    let result = ureq::get("http://ns1.dnspod.net:6666")
+        .timeout(Duration::from_secs(5))
+        .call();
+
+    match result {
+        Ok(r) => println!("{}", r.into_string().unwrap()),
+        Err(e) => println!("{:?}", e),
     }
 }
